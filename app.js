ba074f663ba26b3c429b392a4262c81a42bcbb1a
SergeyKardashev <angeldeep@yandex.ru>
2023-11-25 22:47:13 +0700

    feat: добавить логгеры ошибок и запросов

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const appRouter = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 
@@ -16,9 +17,13 @@ app.use(helmet());
 app.use(cors());
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.use('/', appRouter);
+
+app.use(errorLogger);
 app.use(errorHandler); // my global error handler and sorter for CAUGHT errors
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}, mongoose ${mongoose.version}`);
